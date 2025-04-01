40379b95603329b0050435830b678142dad87a62
Eric Scherer <140315751+EricScherer2006@users.noreply.github.com>
2025-04-01 16:11:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from backend.web_socket import main as websocket_server
 
 def run_sniffer():
     """Runs the packet sniffer in a separate thread."""
+    print("Sniffer thread starting")
     start_sniffing()
-    print("Test print from sniffer")
+    print("Sniffer thread started")
 
 async def main():
     """Runs both the sniffer and WebSocket server concurrently."""
+    print("Main async loop started")
     loop = asyncio.get_running_loop()
 
     # Run sniffer in a background thread (non-blocking)
     loop.run_in_executor(None, run_sniffer)
-
+    print("Starting websocket_server")
     # Start the WebSocket server (async function)
     await websocket_server()
-
-    print("Test print from main loop")  
+    print("Websocket_server started")
 
 # Ensure this runs in the main event loop
 if __name__ == "__main__":
-    asyncio.run(main())  #
+    print("Script started")
+    asyncio.run(main()) 
+    print("Script finished")
