7312e25ec68a3db894bf64cc37fdb8c7c7a21be8
Eric Scherer <140315751+EricScherer2006@users.noreply.github.com>
2025-04-04 12:51:17 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,13 @@
 const protocolsDiv = document.getElementById("protocols");
 const socket = new WebSocket("ws://localhost:8765");
 
+socket.onopen = function() {
+    console.log("WebSocket connected to frontend JS!");
+};
+socket.onerror = function(error) {
+    console.error("WebSocket caused an error:", error);
+};
+
 socket.onmessage = function(event) {
     const data = JSON.parse(event.data);
     protocolsDiv.innerText = `Protocol: ${data.protocol}, Size: ${data.size} bytes`;
