21e98aa4eacf1bc79be92916cb4d6b8d6ddeda91
Eric Scherer <140315751+EricScherer2006@users.noreply.github.com>
2025-04-09 11:43:18 +0200

    Update web_socket.py (#19)

--- backend/web_socket.py
+++ backend/web_socket.py
@@ -14,5 +14,5 @@ async def send_data(websocket, path):
 
 async def start_websocket_server():
     print("Starting WebSocket server at ws://localhost:8765")
-    async with websockets.serve(send_data, "localhost", 8765):
+    async with websockets.serve(send_data, "0.0.0.0", 8765):
         await asyncio.Future()
